9f368319db98324743e2779be93064ea0a066e68
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-08-11 12:11:56 +0530

    Spring Boot
    Spring Boot

---
+++ demo/src/main/java/com/example/demo/Laptop.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	public void compile() {
+		// TODO Auto-generated method stub
+		System.out.println("Compiling");
+		
+	}
+
+}
